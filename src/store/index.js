a3f632505fd17d0e59b8feb8bf5e4675a6319161
lisi <1411452095>
2020-01-11 20:50:31 +0800

    vuex状态管理token

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as auth from '@/utils/auth'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: auth.getUser() // 从缓存中
   },
   mutations: {
+    updateUser (state, payload) {
+      state.user = payload.user
+      auth.setUser(payload.user)
+    },
+    clearUser (state) {
+      state.user = {}
+      auth.delUser()
+    }
   },
   actions: {
   },
